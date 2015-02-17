a3380e2a8574d48d9849afed7ba0a907386c89e2
Shashi Gowda <shashigowda91@gmail.com>
2015-02-17 17:57:44 +0530

    Hotswapping but super buggy :(

--- bin/setup.jl
+++ bin/setup.jl
@@ -7,10 +7,19 @@ import Morsel: prepare_response
 import Patchwork: Elem
 import Canvas: Tile, render
 
+write_patchwork_prelude(io::IO) =
+    write(io, "<script>", Patchwork.js_runtime(), "</script>")
+
+write_canvas_prelude(io::IO) = begin
+    write(io, Canvas.custom_elements())
+    write_patchwork_prelude(io)
+end
+
 function prepare_response{ns, tag}(
         data::Elem{ns, tag}, req::MeddleRequest, res::Response)
     io = IOBuffer()
-    write(io, "<script>", Patchwork.js_runtime(), "</script>")
+    write_patchwork_prelude(io)
+
     writemime(io, MIME"text/html"(), data)
     prepare_response(takebuf_string(io), req, res)
 end
@@ -19,8 +28,9 @@ function prepare_response(
         data::Tile, req::MeddleRequest, res::Response)
 
     io = IOBuffer()
-    write(io, Canvas.custom_elements())
-    write(io, "<script>", Patchwork.js_runtime(), "</script>")
+
+    write_canvas_prelude(io)
+
     writemime(io, MIME"text/html"(), render(data))
     prepare_response(takebuf_string(io), req, res)
 end
