50afbf5d8d512d3236108de819c8d0e5eb2bce42
Shashi Gowda <shashigowda91@gmail.com>
2015-02-20 01:01:27 +0530

    Flow example

---
+++ examples/index.jl
@@ -0,0 +1,28 @@
+
+# An infinite number of mathematicians walk into a bar...
+
+using Canvas
+using Color
+
+colors = distinguishable_colors(9)
+
+box(w, h, n) =
+    (div(" ") & [:style =>
+        ["backgroundColor" => "#" * hex(colors[n % 9 + 1])]]) |> size(w, h)
+
+cut(w, h, dir::Canvas.Up) =
+    (w, h/2)
+cut(w, h, dir::Canvas.Down) =
+    (w, h/2)
+cut(w, h, dir) =
+    (w/2, h)
+
+stack(w, h, d, n) =
+    n == 0 ?
+        empty :
+        flow(d[1], [ box(cut(w, h, d[1])..., n),
+            stack(cut(w, h, d[1])..., vcat(d[2:], d[1]), n-1)])
+
+main = place(middle,
+             stack(80vmin, 80vmin, [down, left, up, right], 15),
+             snugfit())
