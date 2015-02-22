7d88985c44ac15e8b72eba06b48e114d2efda059
Shashi Gowda <shashigowda91@gmail.com>
2015-02-22 15:44:36 +0530

    Refactor Canvasd into a class

--- examples/index.jl
+++ examples/index.jl
@@ -21,8 +21,8 @@ stack(w, h, d, n) =
     n == 0 ?
         empty :
         flow(d[1], [ box(cut(w, h, d[1])..., n),
-            stack(cut(w, h, d[1])..., vcat(d[2:], d[1]), n-1)])
+            stack(cut(w, h, d[1])..., vcat(d[2:end], d[1]), n-1)])
 
 main = place(middle,
-             stack(80vmin, 80vmin, [down, left, up, right], 15),
+             stack(80vmin, 80vmin, [down, left, up, right], 11),
              snugfit())
