bfd29cc101be9ee4c70afbca81f1da593109cf18
Shashi Gowda <shashigowda91@gmail.com>
2015-03-05 02:28:04 +0530

    hskip, vskip take up no area. flow is flex, not inline-flex

--- src/layout.jl
+++ src/layout.jl
@@ -369,5 +369,5 @@ end
 vfill(y, el=div(" ")) = el |> height(100cent)
 hfill(x, el=div(" ")) = el |> width(100cent)
 
-vskip(y) = size(1px, y, empty)
-hskip(x) = size(x, 1px, empty)
+vskip(y) = size(0px, y, empty)
+hskip(x) = size(x, 0px, empty)
