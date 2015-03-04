c214160d85ab207e7e3f0af1af77677b8395df2b
Shashi Gowda <shashigowda91@gmail.com>
2015-03-05 00:28:39 +0530

    samplesignals example: a form

---
+++ examples/form.jl
@@ -0,0 +1,18 @@
+using Canvas
+using Reactive
+
+data = Input(Dict())
+
+main = lift(data) do x
+    samplesignals([:rating, :name], :submit,
+        vbox(
+            h1("Submit your rating"),
+            textinput(label="Your Name", name=:name),
+            hbox("Your rating", slider(0:10, value=10, pin=true, name=:rating)),
+            button("Submit", name=:submit),
+            vskip(1cm),
+            "Submitted data: ",
+            pad(10px, string(x))) |> pad(1inch)
+    ) |> data
+end
+
