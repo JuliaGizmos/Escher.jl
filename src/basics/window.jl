29c89eb0cf1501f784d8eecb829ade5513107cec
Shashi Gowda <shashigowda91@gmail.com>
2015-03-25 18:49:42 +0530

    Fine grained asset management, window is now an argument to main

--- src/basics/window.jl
+++ src/basics/window.jl
@@ -5,14 +5,20 @@ immutable Window
     dimension::Input
     route::Input
     dir::String
-    assets::Set
+    assets::Input
 end
 
 Window(;
     dimension=(0mm, 0mm),
     route="",
     dir="ltr") =
-    Window(Input{Any}(dimension), Input{Any}(route), "ltr", Set{Any}())
+    Window(Input{Any}(dimension), Input{Any}(route), "ltr", Input("basics"))
 
-include_asset(window, asset) =
-    push!(window.assets, asset)
+function resolve_asset(slug)
+    path = Pkg.dir("Canvas", "assets", slug * ".html")
+    if isfile(path)
+        return "/assets/$slug.html"
+    else
+        error("Asset file $path doesn't exist")
+    end
+end
