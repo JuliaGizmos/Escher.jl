60c197067c8e570468b7802025f47fd36e9657e7
Shashi Gowda <shashigowda91@gmail.com>
2015-05-12 19:39:47 +0530

    Towards a more generic cli

---
+++ src/cli/serve.jl
@@ -0,0 +1,184 @@
+#!/usr/bin/env julia
+#
+# Webapp
+using Mux
+using Lazy
+using Escher
+using JSON
+
+using Reactive
+using Patchwork
+
+function loadfile(filename)
+    if isfile(filename)
+
+        try
+            include(filename)
+            return try
+                main
+            catch
+                w -> Elem(:pre, "$filename doesn't have a main function")
+            end
+        catch err
+            bt = backtrace()
+            return (win) -> Elem(:pre, sprint() do io
+                showerror(io, err)
+                Base.show_backtrace(io, bt)
+            end)
+        end
+    else
+        return (w) -> Elem(:p, string(
+            filename, " could not be found."
+        ))
+    end
+end
+
+function setup_socket(file)
+    io = IOBuffer()
+    write(io, """<!doctype html>
+    <html>
+        <head>""")
+    # Include the basics
+    write(io, "<script>", Patchwork.js_runtime(), "</script>")
+    write(io, """<script src="/assets/bower_components/webcomponentsjs/webcomponents.min.js"></script>""")
+    write(io, """<link rel="import" href="$(Escher.resolve_asset("basics"))">""")
+
+    write(io, """</head> <body fullbleed unresolved><div id="root"></div>""")
+    write(io, """<script>window.addEventListener('polymer-ready', function(e) {
+          new Escherd($(JSON.json(file)), "root");
+    })</script>""")
+    takebuf_string(io)
+end
+
+mount_cmd(node, id="root") =
+   [ "command" => "mount",
+    "id" => id,
+    "data" => Patchwork.jsonfmt(node)] |> JSON.json
+
+import_cmd(asset) =
+    [ "command" => "import",
+      "data" => Escher.resolve_asset(asset) ] 
+
+patch_cmd(diff, id="root") =
+   [ "command" => "patch",
+    "id" => id,
+    "data" => Patchwork.jsonfmt(diff)] |> JSON.json
+
+swap!(tilestream, next::Signal) =
+    push!(tilestream, next)
+
+swap!(tilestream, next) =
+    push!(tilestream, Input(next))
+
+const signals = Dict()
+function Escher.setup_transport(x)
+    Escher.makeid(x)
+end
+
+const commands = Dict([
+    ("signal-update", (window, msg) -> begin
+        id = msg["data"]["signalId"]
+        interp, sig = Escher.fromid(id)
+        push!(sig, Escher.interpret(interp, msg["data"]["value"]))
+    end),
+    ("window-size", (window, msg) -> begin
+        dim = (msg["data"][1] * px, msg["data"][1] * px)
+        push!(window.dimension, dim)
+    end),
+    ("window-route", (window, msg) -> begin
+        push!(window.route, msg["data"])
+    end)
+])
+
+
+function uisocket(req)
+    file = abspath(req[:params][:file])
+
+    if !isfile(file)
+        return
+    end
+
+    sock = req[:socket]
+    tilestream = Input{Signal}(Input{Tile}(empty))
+
+    # TODO: Initialize window with session,
+    # window dimensions and what not
+
+    window = Window()
+
+    lift(asset -> write(sock, JSON.json(import_cmd(asset))),
+         window.assets)
+
+    main = loadfile(file)
+
+    current = main(window)
+
+    swap!(tilestream, current)
+
+    rendered = render(current)
+    try
+        write(sock, mount_cmd(rendered))
+    catch ex
+        if isopen(sock)
+            rethrow(ex)
+        end
+    end
+
+    foldl(rendered, flatten(tilestream; typ=Any); typ=Any) do prev, next
+        rendered_next = render(next)
+        try
+            write(sock, patch_cmd(
+                Patchwork.diff(prev, rendered_next)))
+        catch ex
+            if isopen(sock)
+                rethrow(ex)
+            end
+        end
+        rendered_next
+    end
+
+    @async while isopen(sock)
+        data = read(sock)
+
+        msg = JSON.parse(bytestring(data))
+        if !haskey(commands, msg["command"])
+            warn("Unknown command received ", msg["command"])
+        else
+            commands[msg["command"]](window, msg)
+        end
+    end
+
+    while isopen(sock)
+        fw = watch_file(file)
+        wait(fw)
+        close(fw)
+        sleep(0.05)
+
+        main = loadfile(file)
+        next = main(window)
+
+        # Replace the current main signal
+        swap!(tilestream, next)
+    end
+
+end
+
+function escher_serve()
+    # App
+    @app static = (
+        Mux.defaults,
+        route("assets", Mux.files(Pkg.dir("Escher", "assets")), Mux.notfound()),
+        route("/:file", req -> setup_socket(req[:params][:file])),
+        route("/", req -> setup_socket("index.jl")),
+        Mux.notfound(),
+    )
+
+    @app comm = (
+        Mux.wdefaults,
+        route("/socket/:file", uisocket),
+        Mux.wclose,
+        Mux.notfound(),
+    )
+
+    @sync serve(static, comm)
+end
