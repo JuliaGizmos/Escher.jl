892b145fb971a22fe5178946fe29811ecac30be6
Shashi Gowda <shashigowda91@gmail.com>
2015-06-22 04:03:15 +0530

    Now you can have slide-specific transitions

--- src/library/slideshow.jl
+++ src/library/slideshow.jl
@@ -1,15 +1,36 @@
-export slideshow
+export slideshow, slide, fragment
 
 @api slideshow => SlideShow <: Selection begin
     curry(tiles::TileList)
     kwarg(selected::Int=0)
-    kwarg(transitions::String="slide-from-right cross-fade-all")
 end
 
 render_slide(x, state) =
     packitems(center, packacross(center, vbox([x]))) |>
         (t -> render(t, state) & @d(:attributes => @d(:fit => :fit)))
 
+@api slide => Slide <: Tile begin
+    arg(tile::Tile)
+    kwarg(transitions::String="slide-from-right cross-fade-all")
+end
+
+@api fragment => Fragment <: Tile begin
+    typedarg(index::Int=0)
+    curry(tile::Tile)
+end
+
+render(f::Fragment, state) =
+    Elem("slide-fragment", render(f.tile, state),
+        index=f.index)
+
+render(s::Slide, state) =
+    Elem("slide-tile", render(s.tile, state)) & @d(
+        "inTransitions" => s.transitions)
+
+wrapslide(s::Slide) = s
+wrapslide(s) = slide(s)
+
 render(x::SlideShow, state) =
-    Elem("slide-show", map(x -> render_slide(x, state), x.tiles.tiles),
-        transitions=x.transitions)
+    Elem("slide-show",
+        map(x -> render(wrapslide(x), state), x.tiles.tiles))
+
