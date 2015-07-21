5f25b1b529047cad86a837642aee4d3f3c32d7f7
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2015-07-21 23:54:22 +0530

    Docs for typography API functions.

---
+++ docs/typography-api.jl
@@ -0,0 +1,27 @@
+using Color
+using Markdown
+
+include("helpers/page.jl")
+include("helpers/doc.jl")
+
+intro = md"""
+$(title(2, "Typography"))
+$(vskip(1em))
+
+# Functions
+$(vskip(1em))
+
+$(
+   showdocs([fontsize, fontweight, fontcolor, fontstyle, fontfamily, fonttype,
+             fontcase, textalign, lineheight, letterspacing, heading, blockquote,
+             caption, emph, code])
+)
+
+$(vskip(4em))
+
+"""
+
+function main(window)
+    docpage(intro)
+end
+
--- src/basics/typography.jl
+++ src/basics/typography.jl
@@ -1,5 +1,4 @@
 using Color
-
 export plaintext,
        fontfamily,
        fontsize,
@@ -90,8 +89,15 @@ classes(::Lighter) = "font-lighter"
 
 const allowed_font_weights = 100:100:900
 @api fontweight => WithFontWeight{T <: Union(Int, FontWeight)} <: Tile begin
-    arg(weight::T)
-    curry(tiles::TileList)
+    doc("Set the font weight of text in one or more tiles.")
+    arg(
+        weight::T, 
+        doc="Font weight. Valid font weights are multiplies of 100 between 100 and 900."
+    )
+    curry(
+        tiles::TileList,
+        doc="A tile or a vector of tiles."
+    )
 end
 WithFontWeight{T}(weight::T, tile) = WithFontWeight{T}(weight, tile)
 
@@ -106,8 +112,9 @@ render{T <: FontWeight}(t::WithFontWeight{T}, state) =
     addclasses(wrapmany(t.tiles, :span, state), classes(t.weight))
 
 @api fontcolor => FontColor <: Tile begin
-    arg(color::ColorValue)
-    curry(tiles::TileList)
+    doc("Set the font color.")
+    arg(color::ColorValue, doc="The color.")
+    curry(tiles::TileList, doc="A tile or a vector of tiles.")
 end
 
 fontcolor(c::String) = fontcolor(color(c))
@@ -117,8 +124,9 @@ render(t::FontColor, state) =
     wrapmany(t.tiles, :span, state) & style(@d(:color => render_color(t.color)))
 
 @api fontfamily => FontFamily <: Tile begin
-    arg(family::String)
-    curry(tile::TileList)
+    doc("Set the font family.")
+    arg(family::String, doc="The font family")
+    curry(tile::TileList, doc="A tile or a vector of tiles")
 end
 render(t::FontFamily, state) =
     wrapmany(t.tile, :span, state) & style(@d(:fontFamily => t.family))
@@ -132,8 +140,13 @@ abstract FontType
     monospace => Monospace
 end
 @api fonttype => WithFontType <: Tile begin
-    arg(typ::FontType)
-    curry(tiles::TileList)
+    doc("Set the font type.")
+    arg(
+        typ::FontType, 
+        doc=md"""The font type. Valid font types are `serif`, `sanserif`,
+                 `slabserif` and `monospace`."""
+    )
+    curry(tiles::TileList, doc="A tile or a vector of tiles.")
 end
 classes(::Serif) = "font-serif"
 classes(::SansSerif) = "font-sansserif"
@@ -155,8 +168,12 @@ classes(::Slanted) = "font-slanted"
 classes(::Italic) = "font-italic"
 
 @api fontstyle => WithFontStyle <: Tile begin
-    arg(style::FontStyle)
-    curry(tiles::TileList)
+    doc("Set the font style.")
+    arg(
+        style::FontStyle, 
+        doc=md"The font style. Valid font styles are `normal`, `slanted` and `italic`."
+    )
+    curry(tiles::TileList, doc="A tile or a vector of tiles.")
 end
 render(t::WithFontStyle, state) =
     addclasses(wrapmany(t.tiles, :span, state), classes(t.style))
@@ -173,8 +190,9 @@ classes(::Uppercase) = "font-uppercase"
 classes(::Lowercase) = "font-lowercase"
 
 @api fontcase => WithFontCase <: Tile begin
-    arg(case::FontCase)
-    curry(tiles::TileList)
+    doc("Set the font case.")
+    arg(case::FontCase, doc=md"The case. Valid cases are `ucase` and `lcase`")
+    curry(tiles::TileList, doc="A tile or a vector of tiles.")
 end
 
 render(t::WithFontCase, state) =
@@ -190,8 +208,13 @@ abstract TextAlignment
 end
 
 @api textalign => AlignText{T <: TextAlignment} <: Tile begin
-    typedarg(alignment::T)
-    curry(tile::Tile)
+    doc("Set the text alignment.")
+    typedarg(
+        alignment::T,
+        doc=md"""The alignment. Valid alignments are `raggedright`, `raggedleft`,
+                 `justifytext` and `centertext`."""
+    )
+    curry(tile::Tile, doc="A tile.")
 end
 
 render(t::AlignText{RaggedRight}, state) =
@@ -204,15 +227,17 @@ render(t::AlignText{CenterText}, state) =
     render(t.tile, state) & style(@d(:textAlign => :center))
 
 @api lineheight => LineHeight <: Tile begin
-    arg(height::Length)
-    curry(tiles::TileList)
+    doc("Set the height of lines of text.")
+    arg(height::Length, doc="The height.")
+    curry(tiles::TileList, doc="A tile or a vector of tiles.")
 end
 render(t::LineHeight, state) =
     wrapmany(t.tiles, :span, state) & style(@d(:lineHeight => t.height))
 
 @api letterspacing => LetterSpacing <: Tile begin
-    arg(space::Length)
-    curry(tiles::TileList)
+    doc("Set the spacing between letters in text.")
+    arg(space::Length, doc="The spacing.")
+    curry(tiles::TileList, doc="A tile or a vector of tiles.")
 end
 render(t::LetterSpacing, state) =
     wrapmany(t.tiles, :span, state) & style(@d(:letterSpacing => t.space))
@@ -220,6 +245,8 @@ render(t::LetterSpacing, state) =
 # Themable fonts
 
 heading(n::Int, txt) = class("heading-$n", txt, forcewrap=true, wrap=:h1)
+heading(n::Int) = t -> heading(n,t)
+
 title(n::Int, txt) = class("title-$n", txt)
 
 h1(txt) = heading(1, txt)
@@ -227,13 +254,34 @@ h2(txt) = heading(2, txt)
 h3(txt) = heading(3, txt)
 h4(txt) = heading(4, txt)
 
+@apidoc heading => Class <: Tile begin
+    doc(md"Create a heading. You can use `h1`, `h2`, `h3`, `h4` for brevity.") 
+    arg(level::Int, doc="Heading level.")
+    curry(tile::TileList, doc="A tile or a vector of tiles.") 
+end
+
 blockquote(txt) = class("blockquote", txt, forcewrap=true, wrap=:blockquote)
+@apidoc blockquote => Class <: Tile begin
+    doc("Create a quote block.")
+    arg(tile::Tile, doc="The quote.")
+end
+
 caption(txt) = class("caption", txt, wrap=:span)
+@apidoc caption => Class <: Tile begin
+    doc("Create a caption.")
+    arg(tile::Tile, doc="The caption.")
+end
+
 emph(txt) = class("emph", txt, forcewrap=true, wrap=:em)
+@apidoc emph => Class <: Tile begin
+    doc("Create a emphasized text.")
+    arg(tile::Tile, doc="The text to be emphasized.")
+end
 
 @api code => Code <: Tile begin
-    arg(code::Any)
-    kwarg(language::String="julia")
+    doc("Stylize text as code.")
+    arg(code::Any, doc="The code.")
+    kwarg(language::String="julia", doc="The language for syntax highlighting.")
 end
 
 ## String will go inside a span, is this OK?
