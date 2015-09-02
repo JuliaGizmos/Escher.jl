92565024adff152b95e5a5bb8c1c4cf9984a5483
Maxim Grechkin <grechkin@cs.washington.edu>
2015-09-02 15:11:18 -0700

    fix v0.3 compat

--- src/basics/window.jl
+++ src/basics/window.jl
@@ -16,7 +16,7 @@ Window(;
     dir="ltr") =
     Window(Input{Bool}(alive), Input{Any}(dimension), Input{Any}(route), "ltr", Input{Any}("basics"))
 
-resolve_asset(slug, prefix="/assets", joinfn=(x, y) -> x * "/" * y) = begin
+resolve_asset(slug::String, prefix="/assets", joinfn=(x, y) -> x * "/" * y) = begin
     path = Pkg.dir("Escher", "assets", slug * ".html")
     if isfile(path)
         return joinfn(prefix, "$slug.html")
@@ -25,7 +25,7 @@ resolve_asset(slug, prefix="/assets", joinfn=(x, y) -> x * "/" * y) = begin
     end
 end
 
-resolve_asset(tup::Tuple{String,String}, prefix ="/pkg", joinfn=(x, y) -> x * "/" * y) = begin
+resolve_asset(tup :: (@compat Tuple{String, String}), prefix ="/pkg", joinfn=(x, y) -> x * "/" * y) = begin
     pkg = tup[1]
     slug = tup[2]
     path = Pkg.dir(pkg, "assets", slug * ".html")
