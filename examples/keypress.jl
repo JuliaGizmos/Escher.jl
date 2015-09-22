24881b75b4a7b857ad906d221f1dfb870e68a28b
Shashi Gowda <shashigowda91@gmail.com>
2015-09-22 09:38:58 +0530

    Keypress example

---
+++ examples/keypress.jl
@@ -0,0 +1,9 @@
+
+key_input = Input{Key}(nokey)
+
+function main(window)
+    cont = container(40em, 40em) |> fillcolor("#f1f3f1")
+    box = inset(Escher.middle, cont, vbox(md"**Focus and press w, a, s, d or arrow keys**", key_input))
+
+    keypress("w a s d up down left right", box) >>> key_input
+end
