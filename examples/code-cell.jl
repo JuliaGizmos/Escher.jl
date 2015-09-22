df800a97df1598ec24b94a3e333d19ada153a43a
Shashi Gowda <shashigowda91@gmail.com>
2015-09-22 09:23:29 +0530

    Add code cell example

---
+++ examples/code-cell.jl
@@ -0,0 +1,18 @@
+
+function main(window)
+    push!(window.assets, "codemirror")
+    inp = Input(Dict())
+
+    s = sampler() # A thing that lets you watch widgets/behaviors upon updates to other behaviors
+
+    editor = watch!(s, codemirror("some code", name=:code))
+    code_cell = trigger!(s, keypress("ctrl+enter shift+enter", editor))
+
+    plugsampler(s,
+       vbox(
+           "My code cell",
+           code_cell,
+           inp,
+       )
+    ) >>> inp
+end
