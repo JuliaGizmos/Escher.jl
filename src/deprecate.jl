db92e38c99972b7cd4fcbda84d89170f56db3b2b
Shashi Gowda <shashigowda91@gmail.com>
2015-09-22 19:33:24 +0530

    deprecate old dropdownmenu

--- src/deprecate.jl
+++ src/deprecate.jl
@@ -3,3 +3,4 @@ export sampler
 
 @deprecate sample(s::Sampler, t::Tile) plugsampler(s, t)
 @deprecate broadcast(t::Tile) wrapbehavior(t)
+@deprecate dropdownmenu(t::Tile, xs::AbstractArray) dropdownmenu(t, menu(xs))
