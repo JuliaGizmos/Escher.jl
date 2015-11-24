6319cfdf630ba9794edf02a2ef71eb7882f6c23e
Shashi Gowda <shashigowda91@gmail.com>
2015-11-24 12:00:40 +0530

    More deprecations - example fix

--- src/deprecate.jl
+++ src/deprecate.jl
@@ -1,6 +1,10 @@
-import Base.@deprecate
-export sampler
+import Base: @deprecate, broadcast
+export sampler, sample
 
-@deprecate sample(s::Sampler, t::Tile) plugsampler(s, t)
 @deprecate broadcast(t::Tile) wrapbehavior(t)
 @deprecate dropdownmenu(t::Tile, xs::AbstractArray) dropdownmenu(t, menu(xs))
+@deprecate addinterpreter(x, y) intent(x, y)
+@deprecate sample(x::Sampler, t) intent(x, t)
+@deprecate plugsampler(s::Sampler, t::Tile) intent(s, t)
+@deprecate constant(x, y) intent(constant(x), y)
+@deprecate pairwith(x, y) intent(pairwith(x), y)
