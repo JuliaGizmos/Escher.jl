052da27e493231d56ef5d31bd445540e22888f71
andy grosser <dioptre@gmail.com>
2016-02-01 05:53:28 -0800

    working js sync

--- src/library/jsonmirror.jl
+++ src/library/jsonmirror.jl
@@ -1,22 +1,26 @@
 export jsonmirror
+
 @api jsonmirror => (JsonMirror <: Widget) begin
     doc("Sync Json Data between server and client")
-    arg(json::AbstractString="{}", doc="The json to sync.")
-    kwarg(name::Symbol=:_json, doc="The name for the widget")
-    kwarg(oneway::Bool=true, doc="Two way binding or one way(true).")
-    kwarg(id::AbstractString=replace(string("$(rand(1)[1])"),r"[0-9]\.","idx-"), doc="The unique id of the object.")
+    arg(json::AbstractString="", doc="The json to sync.")
+    kwarg(readonly::Bool=true, doc="Two way binding or one way(true).")
+    kwarg(autoNotify::Bool=false, doc="Tell the server on every change automatically? WARNING: This can cause endless recursion.")
+    kwarg(uid::AbstractString=replace(string("$(rand(1)[1])"),r"[0-9]\.","idx-"), doc="The unique id of the object.")
+    kwarg(category::AbstractString="jsonmirror", doc="Object class.")
 end
 
 wrapbehavior(c::JsonMirror) =
-    hasstate(c, name=c.name, attr="immediateValue", trigger="change")
+	hasstate(c, attr="immediateValue", trigger="change")
 
 # Render to virtual DOM
 render(c::JsonMirror, state) =
     Elem("json-mirror",
         attributes = @d(
-            :json=>c.json,
-	    :name=>c.name,
-	    :id=>c.id,
-            "oneway"=>boolattr(c.oneway)
+            :value=>c.json,
+	    "read-only"=>boolattr(c.readonly),
+	    :uid=>c.uid,
+	    :category=>c.category,
+	    :autoNotify=>c.autoNotify
         )
     )
+
