a852f8dadf1a5b4cfdeeff16409fbe5963a5a4fd
Shashi Gowda <shashigowda91@gmail.com>
2016-04-14 00:06:07 +0530

    move asset files

---
+++ pkg/Escher/bower_components/test-fixture/test-fixture-mocha.js
@@ -0,0 +1,39 @@
+/**
+ * @license
+ * Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+(function (Mocha) {
+  function extendInterfaceWithFixture (interfaceName) {
+    var originalInterface = Mocha.interfaces[interfaceName];
+
+    Mocha.interfaces[interfaceName] = function (suite) {
+      originalInterface.apply(this, arguments);
+
+      suite.on('pre-require', function (context, file, mocha) {
+        if (!(context.afterEach || context.teardown)) {
+          return;
+        }
+
+        context.fixture = function (fixtureId, model) {
+          (context.afterEach || context.teardown)(function () {
+            document
+              .getElementById(fixtureId)
+              .restore();
+          });
+
+          return document
+            .getElementById(fixtureId)
+            .create(model);
+        };
+      });
+    };
+  }
+
+  Object.keys(Mocha.interfaces).forEach(extendInterfaceWithFixture);
+})(this.Mocha);
